6c8ca9968322bee4ef4809a4ca3f7d92ce6389ee
emoldino-louis <woosung.choi@onepredict.com>
2024-07-09 12:55:12 +0900

    first commit

---
+++ algorithm/src/main/java/code/algorithm/Main.java
@@ -0,0 +1,15 @@
+package code.algorithm;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String lastString = "??!";
+
+		String s = sc.next();
+		System.out.println(s + lastString);
+
+	}
+}
