cacd9f42a5542f9089a709a7413525f916aa62fb
LooTan_H <82349260+LooSung@users.noreply.github.com>
2024-07-12 13:14:42 +0900

    [Silver IV] Title: 설탕 배달, Time: 172 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,27 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		int cnt = 0;
+		while(n > 0) {
+			if(n % 5  == 0) {
+				cnt += n/5;
+				break;
+			} else {
+				n -= 3;
+				cnt++;
+			}
+			
+			if(n< 0){
+				cnt = -1;
+			}
+		}
+
+		System.out.println(cnt);
+	}
+}
